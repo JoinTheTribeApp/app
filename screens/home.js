d92d300ff29d752666d39a1126636ae4af59d004
Maxxwell <maxxwellfirth@gmail.com>
2020-03-02 22:46:36 +0000

    Version 1 of app

--- screens/home.js
+++ screens/home.js
@@ -27,92 +27,302 @@ export default function Home({ navigation }) {
       avatar: "../assets/star.png",
       venture: "Blacwxlf",
       rating: 1,
-      body: "No notes",
+      body: "None",
       key: "1"
     },
     {
       title: "Adil Dewan",
       venture: "Nuts of Argania",
       rating: 1,
-      body: "No notes",
+      body: "None",
       key: "2"
     },
     {
       title: "Alex McKeeman",
       venture: "Filt",
       rating: 1,
-      body: "No notes",
+      body: "None",
       key: "3"
     },
     {
       title: "Alicia Norton",
       venture: "Treacle Health",
       rating: 4,
-      body: "No notes",
+      body: "None",
       key: "4"
+    },
+    {
+      title: "Ariel Kendall",
+      venture: "Woke Comics",
+      rating: 4,
+      body: "None",
+      key: "5"
     },
     {
       title: "Ash Staines",
       venture: "DoubleTapp",
       rating: 4,
-      body: "No notes",
-      key: "5"
+      body: "None",
+      key: "6"
     },
     {
       title: "Ben Curtis",
       venture: "Aviation Experience Days",
       rating: 4,
-      body: "No notes",
-      key: "6"
+      body: "None",
+      key: "7"
     },
     {
       title: "Claud Williams",
       venture: "Dream Nation",
       rating: 4,
-      body: "No notes",
-      key: "7"
+      body: "None",
+      key: "8"
     },
     {
       title: "Claudine Adeyemi",
       venture: "Career Ear",
       rating: 4,
-      body: "No notes",
-      key: "8"
+      body: "None",
+      key: "9"
     },
     {
       title: "Fleur Colvile",
       venture: "Mix & Rise",
       rating: 4,
-      body: "No notes",
-      key: "9"
+      body: "None",
+      key: "10"
     },
     {
       title: "Guy Wilkinson",
       venture: "Carbon Emissions Reduction",
       rating: 4,
-      body: "No notes",
-      key: "10"
+      body: "None",
+      key: "11"
     },
     {
       title: "Hasan Sukkar",
       venture: "Dogger",
       rating: 4,
-      body: "No notes",
-      key: "11"
+      body: "None",
+      key: "12"
     },
     {
       title: "Hugo Besley",
       venture: "Studio Systems",
       rating: 4,
-      body: "No notes",
-      key: "12"
+      body: "None",
+      key: "13"
+    },
+    {
+      title: "Ina Bierylo",
+      venture: "",
+      rating: 4,
+      body: "None",
+      key: "14"
+    },
+    {
+      title: "Ish Wijewardana",
+      venture: "Strivy",
+      rating: 4,
+      body: "None",
+      key: "15"
+    },
+    {
+      title: "James Nightingall",
+      venture: "HomeFinder",
+      rating: 4,
+      body: "None",
+      key: "16"
+    },
+    {
+      title: "James Stirrat",
+      venture: "CircleOS",
+      rating: 4,
+      body: "None",
+      key: "17"
+    },
+    {
+      title: "Jaron Soh",
+      venture: "Traktion",
+      rating: 4,
+      body: "None",
+      key: "18"
+    },
+    {
+      title: "Joel Gujral",
+      venture: "Firedrop",
+      rating: 4,
+      body: "None",
+      key: "19"
+    },
+    {
+      title: "Karen Meynard",
+      venture: "ACT!VATE",
+      rating: 4,
+      body: "None",
+      key: "20"
+    },
+    {
+      title: "Kat Merkalenko",
+      venture: "Gifter",
+      rating: 4,
+      body: "None",
+      key: "21"
+    },
+    {
+      title: "Kate Nightingale",
+      venture: "Pips",
+      rating: 4,
+      body: "None",
+      key: "22"
+    },
+    {
+      title: "Kiran Kotecha",
+      venture: "LOKAL",
+      rating: 4,
+      body: "None",
+      key: "23"
+    },
+    {
+      title: "Lucia Banjo",
+      venture: "Embrace",
+      rating: 4,
+      body: "None",
+      key: "24"
+    },
+    {
+      title: "Luke Billings",
+      venture: "Kanbanchi Job Tracker",
+      rating: 4,
+      body: "None",
+      key: "25"
+    },
+    {
+      title: "Matthew Holding",
+      venture: "",
+      rating: 4,
+      body: "None",
+      key: "26"
+    },
+    {
+      title: "Max Hampshire",
+      venture: "",
+      rating: 4,
+      body: "None",
+      key: "27"
     },
     {
       title: "Maxxwell Firth",
       venture: "Founder Tribe",
       rating: 5,
-      body: "No notes",
-      key: "13"
+      body: "None",
+      key: "28"
+    },
+    {
+      title: "Melissa Yacoub",
+      venture: "Discover Consultancy",
+      rating: 5,
+      body: "None",
+      key: "29"
+    },
+    {
+      title: "Natasha Eeles",
+      venture: "Bold Voices",
+      rating: 5,
+      body: "None",
+      key: "30"
+    },
+    {
+      title: "Nathalie Stark",
+      venture: "",
+      rating: 5,
+      body: "None",
+      key: "31"
+    },
+    {
+      title: "Nish Modasia",
+      venture: "EN ROOT",
+      rating: 5,
+      body: "None",
+      key: "32"
+    },
+    {
+      title: "Patrick Dobinson",
+      venture: "",
+      rating: 5,
+      body: "None",
+      key: "33"
+    },
+    {
+      title: "Pavel Kroupa",
+      venture: "Carbon Accounting System",
+      rating: 5,
+      body: "None",
+      key: "34"
+    },
+    {
+      title: "Phoebe Jones",
+      venture: "RecoverMe",
+      rating: 5,
+      body: "None",
+      key: "35"
+    },
+    {
+      title: "Sam Ducker",
+      venture: "Anyone",
+      rating: 5,
+      body: "None",
+      key: "36"
+    },
+    {
+      title: "Suraj Tirupati",
+      venture: "",
+      rating: 5,
+      body: "None",
+      key: "37"
+    },
+    {
+      title: "Tara Mahboub",
+      venture: "One Breath London",
+      rating: 5,
+      body: "None",
+      key: "38"
+    },
+    {
+      title: "Tereza Pigova",
+      venture: "1001 Stories",
+      rating: 5,
+      body: "None",
+      key: "39"
+    },
+    {
+      title: "Tobia Charles",
+      venture: "",
+      rating: 5,
+      body: "None",
+      key: "40"
+    },
+    {
+      title: "Victoria Collins",
+      venture: "LingUpp",
+      rating: 5,
+      body: "None",
+      key: "41"
+    },
+    {
+      title: "Yasmine Manshadi",
+      venture: "Persian Supermarket",
+      rating: 5,
+      body: "None",
+      key: "42"
+    },
+    {
+      title: "Zac Feighery",
+      venture: "YouDrive",
+      rating: 5,
+      body: "None",
+      key: "43"
     }
   ]);
 
--- screens/todoDetails.js
+++ screens/todoDetails.js
@@ -9,13 +9,19 @@ export default function TodoDetails({ navigation }) {
   return (
     <View style={globalStyles.container}>
       <Card>
-        <Text>{navigation.getParam("title")}</Text>
-        <Text>{navigation.getParam("body")}</Text>
-        <View style={StyleSheet.rating}>
-          <Text>Member Type: </Text>
-          <Image source={images.ratings[rating]} />
-          <Text>Avatar: </Text>
-          <Image style={styles.pics} source={images.avatars[key]} />
+        <Image style={styles.avatars} source={images.avatars[key]} />
+        <Text style={styles.profileHeader}>Name:</Text>
+        <Text style={styles.profileText}>{navigation.getParam("title")}</Text>
+        <Text style={styles.profileHeader}>Venture:</Text>
+        <Text style={styles.profileText}>{navigation.getParam("venture")}</Text>
+        <View style={styles.profileText} style={StyleSheet.rating}>
+          <Text style={styles.profileHeader}>Membership:</Text>
+          <Text style={styles.profileText}>
+            Premium Member&nbsp;
+            <Image style={styles.stars} source={images.ratings[rating]} />
+          </Text>
+          <Text style={styles.profileHeader}>Notes:</Text>
+          <Text style={styles.profileText}>{navigation.getParam("body")}</Text>
         </View>
       </Card>
     </View>
@@ -23,8 +29,23 @@ export default function TodoDetails({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  pics: {
-    width: 250,
-    height: 250
+  avatars: {
+    width: 330,
+    height: 330,
+    right: 0
+  },
+  profileHeader: {
+    fontSize: 16,
+    marginTop: 5,
+    marginLeft: 5,
+    fontWeight: "bold"
+  },
+  profileText: {
+    margin: 5,
+    fontSize: 14
+  },
+  stars: {
+    height: 15,
+    width: 15
   }
 });
